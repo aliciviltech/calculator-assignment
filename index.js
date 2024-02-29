b24b4f4f83a85f2e54724a2ec60aeaa6d976e8d7
aliciviltech <149861352+aliciviltech@users.noreply.github.com>
2024-02-29 18:04:57 +0500

    html and JS files

---
+++ index.js
@@ -0,0 +1,13 @@
+let number1 = parseInt(prompt('                                             "Calculator"\nEnter Number1'));
+let number2 = parseInt(prompt('                                              "Calculator"\nEnter Number2'));
+alert(`        sum of two numbers =  ${number1+number2}
+        subtraction of two numbers = ${number1-number2}
+        multiplication of two number = ${number1*number2}
+        division of two number = ${number1/number2}
+        Remainder of two  number = ${number1%number2}`)
+
+console.log(`        sum of two numbers =  ${number1+number2}
+        subtraction of two numbers = ${number1-number2}
+        multiplication of two number = ${number1*number2}
+        division of two number = ${number1/number2}
+        Remainder of two  number = ${number1%number2}`)        
